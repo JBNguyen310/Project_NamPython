f6e42d6c2e99883621ebb345db724f653e098c19
Nguyễn Hải Nam <hainamdsv14@gmail.com>
2026-04-24 22:42:46 +0700

    compress_code

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,18 @@
-print("HELLO WORLD")
+original_data = "AAABBBCCAA"
+
+
+def compress(data):
+    compressed = []
+    count = 1
+    for i in range(len(data) - 1):
+        if data[i] == data[i + 1]:
+            count += 1
+        else:
+            compressed.append(f"{data[i]}{count}")
+            count = 1
+    compressed.append(f"{data[-1]}{count}")
+    return "".join(compressed)
+
+
+compressed_data = compress(original_data)
+print(compressed_data)
